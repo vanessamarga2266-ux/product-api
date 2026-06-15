4eca66432700dc4c65e09114b5b7c514dc848c4d
JavierEVP04 <javiervillauni@gmail.com>
2026-06-15 08:20:50 -0600

    Update IProductService.java

--- src/main/java/com/ucr/product_api/services/IProductService.java
+++ src/main/java/com/ucr/product_api/services/IProductService.java
@@ -3,7 +3,10 @@ package com.ucr.product_api.services;
 import java.util.List;
 
 import com.ucr.product_api.Entities.Product;
+import com.ucr.product_api.dtos.ProductRequestDto;
 
 public interface IProductService {
     List<Product> getAll();
+
+    Product addProduct(ProductRequestDto productDto);
 }
