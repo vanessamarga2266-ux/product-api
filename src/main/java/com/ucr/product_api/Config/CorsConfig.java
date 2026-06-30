208b5581e1404d387a0afcd532c60d606a6eec3f
JavierEVP04 <javiervillauni@gmail.com>
2026-06-30 12:52:21 -0600

    ACTUALIZACION CORS

--- src/main/java/com/ucr/product_api/Config/CorsConfig.java
+++ src/main/java/com/ucr/product_api/Config/CorsConfig.java
@@ -14,10 +14,12 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5174");
-        config.addAllowedOrigin("http://localhost:3000");
-
-        config.addAllowedOrigin("https://front-end-sigma-lime.vercel.app");
+        
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://front-end-sigma-lime.vercel.app"
+        ));
         
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
