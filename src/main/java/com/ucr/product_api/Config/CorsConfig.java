171562de9fb4c0f856a1e6b0a1d91c693258ff61
JavierEVP04 <javiervillauni@gmail.com>
2026-06-30 12:39:31 -0600

    CONEXION DE CORS

--- src/main/java/com/ucr/product_api/Config/CorsConfig.java
+++ src/main/java/com/ucr/product_api/Config/CorsConfig.java
@@ -16,6 +16,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5174");
         config.addAllowedOrigin("http://localhost:3000");
+
+        config.addAllowedOrigin("https://front-end-sigma-lime.vercel.app");
         
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
